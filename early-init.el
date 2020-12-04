dbdc72e04b5bd705bd76b2475824e91236f1da3a
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2020-12-04 15:30:54 +0100

    Improve profiling code in early init.
    Add profiler calls in --debug-init and use internal timer variables and
    a more accurate timing.

--- early-init.el
+++ early-init.el
@@ -13,37 +13,40 @@
 ;;
 ;;; Code:
 
-(defconst my/start-time (current-time))
-
-(defmacro mt (&rest body)
-  "Measure the time it takes to evaluate BODY."
-  `(let ((time (current-time)))
-     ,@body
-     (message "%.06f" (float-time (time-since time)))))
+;; Profiling since here when in debug-mode
+(when init-file-debug
+  (profiler-start 'cpu)
+  (add-hook 'window-setup-hook #'profiler-stop 0))
 
 (defvar file-name-handler-alist-old file-name-handler-alist)
 
-(setq file-name-handler-alist nil
-      message-log-max 16384
-      gc-cons-threshold most-positive-fixnum   ;; Defer Garbage collection
-      gc-cons-percentage 1.0)
+(setq-default file-name-handler-alist nil
+	      message-log-max 16384
+	      gc-cons-threshold most-positive-fixnum   ;; Defer Garbage collection
+	      gc-cons-percentage 1.0)
 
+;; This hook is always added, set to 90 to go to the end
 (add-hook 'window-setup-hook
           (lambda ()
             (setq file-name-handler-alist file-name-handler-alist-old
-                  gc-cons-threshold 800000
-                  gc-cons-percentage 0.1)
-	    (garbage-collect)
-	    (message "Load time %.06f" (float-time (time-since my/start-time))))
-	  t)
+		  gc-cons-threshold 800000
+		  gc-cons-percentage 0.1)
+	    ;; (garbage-collect)
+	    (let ((curtime (current-time)))
+
+	      (message "Times: init:%.06f total:%.06f gc-done:%d"
+		       (float-time (time-subtract after-init-time before-init-time))
+		       (float-time (time-subtract curtime before-init-time))
+		       gcs-done)))
+	  90)
 
 (if (fboundp 'tool-bar-mode)
     (tool-bar-mode   -1))
 
 (if (fboundp 'scroll-bar-mode)
     (scroll-bar-mode -1))
-(menu-bar-mode   -1)
 
+(menu-bar-mode   -1)
 (tooltip-mode    -1)			;; Tool tip in the echo
 (electric-indent-mode -1)
 ;;(flymake-mode    -1)
